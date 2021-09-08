f8b03620d40daef3c6416259c46a70433db0c0eb
IgorPonciano <igorponciano.work@gmail.com>
2021-09-08 20:55:58 -0300

    Changes 08/09
    Changes on scripts and level mock up.
    
    - Added scenes for camera and movement testing, and for initial level tests.
    - Added the Lamp asset, to start prototyping with it.
    - Navmesh bake test on the graveyard.

---
+++ 6_Semestre/Assets/Scripts/NavMeshFollow.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavMeshFollow : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    [SerializeField] private Transform followTransform;
+
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        agent.destination = followTransform.position;
+    }
+}
