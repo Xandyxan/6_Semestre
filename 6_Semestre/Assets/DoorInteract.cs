8816aeb8c88fc07f371f9637b5080ae3336587fd
Xandyxan <79609879+Xandyxan@users.noreply.github.com>
2021-11-26 00:00:39 -0300

    Introduction Area Added

---
+++ 6_Semestre/Assets/DoorInteract.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DoorInteract : MonoBehaviour, ISelectable, IInteractable 
+{
+    [HideInInspector] public bool canUse;
+    [SerializeField] GameObject textFeedback;
+    [SerializeField] private int sceneToLoad;
+    [SerializeField] private DialogueManager2 dialogueManager;
+    [SerializeField] private int dialogueIndex;
+
+    public void Deselect()
+    {
+        textFeedback.SetActive(false);
+    }
+
+    public void Interact()
+    {
+        if(canUse)
+        {
+            SceneManager.LoadScene(sceneToLoad);
+        }
+        else
+        {
+            dialogueManager.ExecuteDialogue(dialogueIndex);
+        }
+    }
+
+    public void Select()
+    {
+        textFeedback.SetActive(true);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
